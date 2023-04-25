c39e9ca357d1be1853be52ff7d111fc6d8ad5d54
Andres Buitrago <buitrago001@gmail.com>
2023-04-24 20:29:19 -0500

    Add files via upload
    login and usemain updates

--- JS/login.js
+++ JS/login.js
@@ -1,7 +1,10 @@
-function logIn(){
+const signUpButton = document.getElementById(signUp);
+const signInButton = document.getElementById(signIp);const container = document.getElementById( container);
 
-}
+signUpButton.addEventListener('click', ()=>{
+    container.classList.add("right-panel-active");
+})
 
-function signUp(){
-    
-}
+signInButton.addEventListener('click', ()=>{
+    container.classList.remove("right-panel-active");
+})
